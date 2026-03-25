ab9467fef65c2271304599537164a2f7e91b4cfd
jassonf478-del <jassonf478@gmail.com>
2026-03-25 15:59:43 -0600

    Prueba de vida de la web

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+  const MaterialApp(
+    home: Scaffold(body: Center(child: Text('¡Web Funcionando!'))),
+  ),
+);
 
 void main() => runApp(const MaterialApp(home: SimpleGallery()));
 
